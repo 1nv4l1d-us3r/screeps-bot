b42c406a3c76bd51d16adc36aecc2c912cda8095
1nv4l1d-us3r <testaccforcolab@gmail.com>
2026-02-23 01:20:00 +0530

    Enhance: update the cpu profiler to hande few edge cases

--- src/helpers/cpuProfiler.ts
+++ src/helpers/cpuProfiler.ts
@@ -1,37 +1,48 @@
-
 interface ProfileFunctionParams {
     name: string;
     func: () => void;
 }
 
-
 export class CpuProfiler {
-    static  profiles: Record<string, number> = {};
+    static profiles: Record<string, number> = {};
+    static currentTick: number = -1;
+
+    private static resetProfiles() {
+        this.profiles = {};
+        this.currentTick = Game.time;
+    }
 
     public static log(name: string) {
-        const now = Game.cpu.getUsed();
-        this.profiles[name] = now;
+        if (this.currentTick !== Game.time) {
+            this.resetProfiles();
+        }
+
+        if (name in this.profiles) {
+            console.error(`Profile ${name} already exists, skipping`);
+            return;
+        }
+
+        const currentCpuUsage = Game.cpu.getUsed();
+        this.profiles[name] = currentCpuUsage;
     }
 
     public static logEnd(name: string) {
-        const startTime = this.profiles[name];
-        if(!startTime) {
+        const startCpuUsage = this.profiles[name];
+
+        if (startCpuUsage === undefined) {
             console.error(`Profile ${name} not found`);
             return;
         }
-        const endTime = Game.cpu.getUsed();
-        const cpuUsage = Math.round((endTime - startTime)*100)/100;
-        console.log(`CPU Usage [${name}]: ${cpuUsage}`);
+
+        const cpuUsage = Game.cpu.getUsed() - startCpuUsage;
+        console.log(`CPU Usage [${name}]: ${cpuUsage.toFixed(2)}`);
+
         delete this.profiles[name];
     }
 
-   
-
-    public static profileFunction(params: ProfileFunctionParams) {
-        const { name, func } = params;
+    public static profileFunction({ name, func }: ProfileFunctionParams) {
         this.log(name);
         func();
         this.logEnd(name);
     }
-
 }
