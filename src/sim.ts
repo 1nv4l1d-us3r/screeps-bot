afb052b7a83930d96125d576706a9204f73f6689
1nv4l1d-us3r <testaccforcolab@gmail.com>
2026-03-15 04:30:40 +0530

    feat: enhance simulation loop with room storage check and logging

--- src/sim.ts
+++ src/sim.ts
@@ -1,10 +1,29 @@
-
-
+import { getAdjacentCoords, getCoordsInRange, getRightCoord, packCoord, unpackCoord } from "geometry";
+import { PackedCoord } from "types/geometry";
+import { Coord} from "types/geometry";
+import { getCoordDistance } from "geometry";
 
 export const loop = () => {
 
+    console.log('sim loop');
 
+    const simRoom=Game.rooms['sim'];
+
+    const roomController=simRoom.controller;
+
+    const storage=simRoom.storage;
+    if(!storage){
+        console.log('no storage');
+        return;
+    }
+
+    const baseCoords=storage.pos
+
+    // const upgraderContainerSpot = getUpgraderContainerSpot(roomController,baseCoords);
 
 
     
 }
+
+
+
