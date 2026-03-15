56fef065bab3df3d79774a9b6ea12d363b65b8de
1nv4l1d-us3r <testaccforcolab@gmail.com>
2026-03-16 04:10:41 +0530

    refactor: remove unused CacheProvider class and related constants to streamline cache management

--- src/helpers/cache.ts
+++ src/helpers/cache.ts
@@ -23,12 +23,12 @@ export const TickCache = <I,O>(key:(input:I)=>string):CacheDecorator<I,O>=>{
         descriptor
     )=>{
         const originalMethod=descriptor.value as (input:I)=>O
-        const cacheMethod=(input:I):O=>{
+        const cacheMethod=function(this:any,input:I):O{
             const cacheKey=key(input)+Game.time
             if(cacheKey===lastCacheKey && cacheValue!==undefined) {
                 return cacheValue
             }
-            cacheValue=originalMethod.call(target,input)
+            cacheValue=originalMethod.call(this,input)
             lastCacheKey=cacheKey
             return cacheValue
         }
