888eb833d025c7e17cbc19b1ecab0ad02d2179e7
1nv4l1d-us3r <testaccforcolab@gmail.com>
2026-05-02 17:48:33 +0530

    refactor: update mining site configuration access in ConstructionManager for improved clarity

--- src/room/managers/constructionManager.ts
+++ src/room/managers/constructionManager.ts
@@ -231,7 +231,7 @@ export class ConstructionManager {
 
         // ----------------- Mining Utility Structures Construction ----------------//
 
-        for(const miningSiteConfig of miningConfig) {
+        for(const miningSiteConfig of miningConfig.miningSites) {
             const { storageCoord, storageType,extractorCoord } = miningSiteConfig;
             if(storageCoord && storageType) {
                 const storageCoordPacked=packCoord(storageCoord);
